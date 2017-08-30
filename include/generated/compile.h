fd0c1f43f369dfda2fbb4578316788661454257b
GameTheory- <game.theory7@hotmail.com>
2017-08-30 14:28:12 -0400

    Clean source

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Fri Jul 28 16:36:58 CEST 2017"
-#define LINUX_COMPILE_BY "xNN"
-#define LINUX_COMPILE_HOST "XeonE5-2670"
-#define LINUX_COMPILER "gcc version 4.8.5 (UBERTC-4.8.5) "
